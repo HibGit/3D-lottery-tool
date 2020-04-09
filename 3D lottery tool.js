d46fd0af41a70f4c2ef8f3c7e543e3623a81e735
HibGit <60831609+HibGit@users.noreply.github.com>
2020-04-09 16:20:37 -0400

    Add files via upload

---
+++ 3D lottery tool.js
@@ -0,0 +1,216 @@
+function make() {
+    while (a == b || a == c || a == d || a == e || a == f || a == g ||
+     b == c || b == d || b == e || b == f || b == g || 
+     c == d || c == e || c == f || c == g ||
+     d == e || d == f || d == g || 
+     e == f || e == g || f == g)  {
+    var a = Math.floor((Math.random() * 50) + 1);
+    var b = Math.floor((Math.random() * 50) + 1);
+    var c = Math.floor((Math.random() * 50) + 1);
+    var d = Math.floor((Math.random() * 50) + 1);
+    var e = Math.floor((Math.random() * 50) + 1);
+    var f = Math.floor((Math.random() * 50) + 1);
+    var g = Math.floor((Math.random() * 50) + 1);
+  }
+    var h = "&#1497";
+    var i = "&#1500";
+    var j = "&#1494";
+    var k = "&#1502";
+    var l = "&#1500";
+    var text = ""+a+", "+b+", "+c+", "+d+", "+e+", "+f+", "+g+"";
+    var x = [];
+    x[0] = a;
+    x[1] = b;
+    x[2] = c;
+    x[3] = d;
+    x[4] = e;
+    x[5] = f;
+    x[6] = g;
+
+        document.getElementsByClassName("plane one")[0].innerHTML = '';
+        document.getElementsByClassName("plane one")[0].innerHTML = a;
+        document.getElementsByClassName("plane two")[0].innerHTML = '';
+        document.getElementsByClassName("plane two")[0].innerHTML = b;
+        document.getElementsByClassName("plane three")[0].innerHTML = '';
+        document.getElementsByClassName("plane three")[0].innerHTML = c;
+        document.getElementsByClassName("plane four")[0].innerHTML = '';
+        document.getElementsByClassName("plane four")[0].innerHTML = d;
+        document.getElementsByClassName("plane five")[0].innerHTML = '';
+        document.getElementsByClassName("plane five")[0].innerHTML = e;
+        document.getElementsByClassName("plane six")[0].innerHTML = '';
+        document.getElementsByClassName("plane six")[0].innerHTML = f;
+        document.getElementsByClassName("plane seven")[0].innerHTML = '';
+        document.getElementsByClassName("plane seven")[0].innerHTML = g;
+        document.getElementsByClassName("plane eight")[0].innerHTML = '';
+        document.getElementsByClassName("plane eight")[0].innerHTML = h;
+        document.getElementsByClassName("plane nine")[0].innerHTML = '';
+        document.getElementsByClassName("plane nine")[0].innerHTML = i;
+        document.getElementsByClassName("plane ten")[0].innerHTML = '';
+        document.getElementsByClassName("plane ten")[0].innerHTML = j;
+        document.getElementsByClassName("plane eleven")[0].innerHTML = '';
+        document.getElementsByClassName("plane eleven")[0].innerHTML = k;
+        document.getElementsByClassName("plane twelve")[0].innerHTML = '';
+        document.getElementsByClassName("plane twelve")[0].innerHTML = l;
+      
+        numberSort = function (a,b) {return a - b};
+        x.sort(numberSort);
+        var message = "MAX numbers:<br> "+x+"";
+        document.getElementById("numbers"). innerHTML = message;
+    }
+  
+  function make649() {
+    while (a == b || a == c || a == d || a == e || a == f || a == g ||
+     b == c || b == d || b == e || b == f || b == g || 
+     c == d || c == e || c == f || c == g ||
+     d == e || d == f || d == g || 
+     e == f )  {
+      var a = Math.floor((Math.random() * 49) + 1);
+    var b = Math.floor((Math.random() * 49) + 1);
+    var c = Math.floor((Math.random() * 49) + 1);
+    var d = Math.floor((Math.random() * 49) + 1);
+    var e = Math.floor((Math.random() * 49) + 1);
+    var f = Math.floor((Math.random() * 49) + 1);
+  }
+    var g = "&#1511";
+    var h = "&#1504";
+    var i = "&#1506";
+    var j = "&#1500";
+    var k = "&#1494";
+    var l = "&#1502";
+    var text = ""+a+", "+b+", "+c+", "+d+", "+e+", "+f+"";
+    var x = [];
+    x[0] = a;
+    x[1] = b;
+    x[2] = c;
+    x[3] = d;
+    x[4] = e;
+    x[5] = f; 
+
+        document.getElementsByClassName("plane one")[0].innerHTML = '';
+        document.getElementsByClassName("plane one")[0].innerHTML = a;
+        document.getElementsByClassName("plane two")[0].innerHTML = '';
+        document.getElementsByClassName("plane two")[0].innerHTML = b;
+        document.getElementsByClassName("plane three")[0].innerHTML = '';
+        document.getElementsByClassName("plane three")[0].innerHTML = c;
+        document.getElementsByClassName("plane four")[0].innerHTML = '';
+        document.getElementsByClassName("plane four")[0].innerHTML = d;
+        document.getElementsByClassName("plane five")[0].innerHTML = '';
+        document.getElementsByClassName("plane five")[0].innerHTML = e;
+        document.getElementsByClassName("plane six")[0].innerHTML = '';
+        document.getElementsByClassName("plane six")[0].innerHTML = f;
+        document.getElementsByClassName("plane seven")[0].innerHTML = '';
+        document.getElementsByClassName("plane seven")[0].innerHTML = g;
+        document.getElementsByClassName("plane eight")[0].innerHTML = '';
+        document.getElementsByClassName("plane eight")[0].innerHTML = h;
+        document.getElementsByClassName("plane nine")[0].innerHTML = '';
+        document.getElementsByClassName("plane nine")[0].innerHTML = i;
+        document.getElementsByClassName("plane ten")[0].innerHTML = '';
+        document.getElementsByClassName("plane ten")[0].innerHTML = j;
+        document.getElementsByClassName("plane eleven")[0].innerHTML = '';
+        document.getElementsByClassName("plane eleven")[0].innerHTML = k;
+        document.getElementsByClassName("plane twelve")[0].innerHTML = '';
+        document.getElementsByClassName("plane twelve")[0].innerHTML = l;
+
+        numberSort = function (a,b) {return a - b};
+        x.sort(numberSort);
+        var message = "6/49 numbers:<br> "+x+"";
+        document.getElementById("numbers"). innerHTML = message;
+    }
+  
+  function makeO45() {
+    while (a == b || a == c || a == d || a == e || a == f || a == g ||
+     b == c || b == d || b == e || b == f || b == g || 
+     c == d || c == e || c == f || c == g ||
+     d == e || d == f || d == g || 
+     e == f )  {
+    var a = Math.floor((Math.random() * 45) + 1);
+    var b = Math.floor((Math.random() * 45) + 1);
+    var c = Math.floor((Math.random() * 45) + 1);
+    var d = Math.floor((Math.random() * 45) + 1);
+    var e = Math.floor((Math.random() * 45) + 1);
+    var f = Math.floor((Math.random() * 45) + 1);
+  }
+    var g = "&#1511";
+    var h = "&#1504";
+    var i = "&#1506";
+    var j = "&#1500";
+    var k = "&#1494";
+    var l = "&#1502";
+    var text = ""+a+", "+b+", "+c+", "+d+", "+e+", "+f+"";
+    var x = [];
+    x[0] = a;
+    x[1] = b;
+    x[2] = c;
+    x[3] = d;
+    x[4] = e;
+    x[5] = f;
+    
+        document.getElementsByClassName("plane one")[0].innerHTML = '';
+        document.getElementsByClassName("plane one")[0].innerHTML = a;
+        document.getElementsByClassName("plane two")[0].innerHTML = '';
+        document.getElementsByClassName("plane two")[0].innerHTML = b;
+        document.getElementsByClassName("plane three")[0].innerHTML = '';
+        document.getElementsByClassName("plane three")[0].innerHTML = c;
+        document.getElementsByClassName("plane four")[0].innerHTML = '';
+        document.getElementsByClassName("plane four")[0].innerHTML = d;
+        document.getElementsByClassName("plane five")[0].innerHTML = '';
+        document.getElementsByClassName("plane five")[0].innerHTML = e;
+        document.getElementsByClassName("plane six")[0].innerHTML = '';
+        document.getElementsByClassName("plane six")[0].innerHTML = f;
+        document.getElementsByClassName("plane seven")[0].innerHTML = '';
+        document.getElementsByClassName("plane seven")[0].innerHTML = g;
+        document.getElementsByClassName("plane eight")[0].innerHTML = '';
+        document.getElementsByClassName("plane eight")[0].innerHTML = h;
+        document.getElementsByClassName("plane nine")[0].innerHTML = '';
+        document.getElementsByClassName("plane nine")[0].innerHTML = i;
+        document.getElementsByClassName("plane ten")[0].innerHTML = '';
+        document.getElementsByClassName("plane ten")[0].innerHTML = j;
+        document.getElementsByClassName("plane eleven")[0].innerHTML = '';
+        document.getElementsByClassName("plane eleven")[0].innerHTML = k;
+        document.getElementsByClassName("plane twelve")[0].innerHTML = '';
+        document.getElementsByClassName("plane twelve")[0].innerHTML = l;
+
+        numberSort = function (a,b) {return a - b};
+        x.sort(numberSort);
+        var message = "O/45 numbers:<br> "+x+"";
+        document.getElementById("numbers"). innerHTML = message;
+   }
+
+  function hasClassName(inElement, inClassName)
+      {
+      var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)');
+      return regExp.test(inElement.className);
+    }
+
+  function addClassName(inElement, inClassName)
+      {
+      if (!hasClassName(inElement, inClassName))
+            inElement.className = [inElement.className, inClassName].join(' ');
+    }
+
+  function removeClassName(inElement, inClassName)
+      {
+        if (hasClassName(inElement, inClassName)) {
+            var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)', 'g');
+            var curClasses = inElement.className;
+            inElement.className = curClasses.replace(regExp, ' ');
+        }
+    }
+
+  function toggleShape()
+      {
+      var shape = document.getElementById('shape');
+          if (hasClassName(shape, 'ring')) {
+          removeClassName(shape, 'ring');
+          addClassName(shape, 'cube');
+          } else {
+          removeClassName(shape, 'cube');
+          addClassName(shape, 'ring');
+      }
+          
+      var stage = document.getElementById('stage');
+      if (hasClassName(shape, 'ring'))
+        stage.style.webkitTransform = 'translateZ(-200px)';
+      else
+        stage.style.webkitTransform = '';
+    }
